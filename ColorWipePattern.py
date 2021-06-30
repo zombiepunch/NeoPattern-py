b928f444b41089880ea1174799f965bdffbcaf1d
nolans <nolanstacey18@gmail.com>
2021-06-30 11:41:59 -0700

    updated color constant names and fixed last pixel not filling in reverse direction for ColorWipePattern

--- ColorWipePattern.py
+++ ColorWipePattern.py
@@ -1,6 +1,6 @@
 import neopixel
 
-from NeoPatternBase import Direction, NeoPatternBase, PIXEL_OFF
+from NeoPatternBase import Direction, NeoPatternBase, Colors
 
 class ColorWipePattern(NeoPatternBase):
     # class constructor
@@ -9,20 +9,25 @@ class ColorWipePattern(NeoPatternBase):
                  interval, 
                  callback, 
                  color_1, 
-                 color_2=PIXEL_OFF, 
+                 color_2=Colors.OFF, 
                  direction=Direction.FORWARD, 
                  brightness=0.1):
-        super().__init__(neopixel, callback, brightness)
+        super().__init__(neopixel, callback, brightness, direction)
         self.interval = interval
+        self.total_steps = self.num_pixels * 2
+        self.idx = 0 if direction == Direction.FORWARD else self.num_pixels
         self.color_1 = color_1
         self.color_2 = color_2
-        
-        if direction != self.direction:
-            self.reverse()
 
 
     def _update_pattern(self):
-        if self.idx >= self.num_pixels:
-            self[self.idx % self.num_pixels] = self.color_2
+        if self.direction == Direction.FORWARD:
+            if self.idx >= self.num_pixels:
+                self[self.idx % self.num_pixels] = self.color_2
+            else:
+                self[self.idx] = self.color_1
         else:
-            self[self.idx] = self.color_1
+            if self.idx > self.num_pixels:
+                self[(self.idx - 1) % self.num_pixels] = self.color_1
+            else:
+                self[(self.idx - 1)] = self.color_2
